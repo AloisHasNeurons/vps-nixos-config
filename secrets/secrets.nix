4b880ea930a7e98588fd36325810c31d75e0e296
Aloïs VINCENT <alois.vincent@imt-atlantique.net>
2026-01-01 15:11:38 +0100

    chore(secrets): update agenix secrets with vps host key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
   alois = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2uzDX8j0gCkpfmB+G9HU3PEEOGp02Nfh4FcIlQ+EWb alois.vincent@imt-atlantique.net";
+  # Host key for the Hetzner VPS (retrieved via ssh-keyscan)
+  vps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILOEPCQcoeIdNqGN5vPjw2bfqslGoKkdcgJnL0u3IC0T";
   alois_laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINa1VOKGJI/j5mfvo5QsKk/tX+vNr3CdjdYYNfbPxdDK alois@fedora";
-  users = [ alois alois_laptop ];
+  users = [ alois alois_laptop vps ];
 in
 {
   "wireguard-private-key.age".publicKeys = users;
