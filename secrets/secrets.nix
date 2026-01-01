8585b300a86397c107ce4a41adb10ca951c387d3
Aloïs VINCENT <alois.vincent@imt-atlantique.net>
2026-01-01 15:36:29 +0100

    chore: rekey secrets for new server

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   alois = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2uzDX8j0gCkpfmB+G9HU3PEEOGp02Nfh4FcIlQ+EWb alois.vincent@imt-atlantique.net";
   # Host key for the Hetzner VPS (retrieved via ssh-keyscan)
-  vps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH7wEbCe9tAhIimuXYp5+4qfTddmL+/wuhm2hiAE65Um";
+  vps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDvkNs+V1aEChmpKJ+0IFiQvPTD/YQZBrH17exMiQyy";
   alois_laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINa1VOKGJI/j5mfvo5QsKk/tX+vNr3CdjdYYNfbPxdDK alois@fedora";
   users = [ alois alois_laptop vps ];
 in
