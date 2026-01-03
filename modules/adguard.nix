3053fbf94e9a4389380983aec985f31882844dfc
Aloïs VINCENT <alois.vincent@imt-atlantique.net>
2026-01-03 14:09:33 +0100

    modules: added a declarative configuration for adguard home.

--- modules/adguard.nix
+++ modules/adguard.nix
@@ -11,10 +11,44 @@
         address = "127.0.0.1:3000"; # Web UI only on localhost (proxied by nginx)
       };
       dns = {
-        # Only bind DNS on localhost and WireGuard interface - NOT public!
         bind_hosts = ["127.0.0.1" "10.100.0.1"];
         port = 53;
-        upstream_dns = ["1.1.1.1" "8.8.8.8"];
+        # Parallel upstream requests for speed
+        upstream_dns = [
+          "https://dns.cloudflare.com/dns-query"
+          "https://dns.google/dns-query"
+          "https://dns.quad9.net/dns-query"
+        ];
+        bootstrap_dns = ["1.1.1.1" "8.8.8.8" "9.9.9.9"];
+        rate_limit = 0; # Unlimited
+      };
+      filtering = {
+        safe_browsing = true; # Block phishing/malware
+        safe_search = {
+          enabled = false;
+        };
+      };
+      filters = [
+        {
+          enabled = true;
+          url = "https://big.oisd.nl"; # OISD Big (Comprehensive, low false positives)
+          name = "OISD Big";
+          id = 1;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt"; # AdGuard DNS filter
+          name = "AdGuard DNS filter";
+          id = 2;
+        }
+      ];
+      statistics = {
+        enabled = true;
+        interval = "24h"; # retention
+      };
+      querylog = {
+        enabled = true;
+        interval = "2160h"; # 90 days retention
       };
     };
   };
