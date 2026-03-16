cc265830bd30cf82517f873f18564f0ad121f72a
Aloïs VINCENT <alois.vincent@imt-atlantique.net>
2026-03-16 22:28:59 +0100

    feat(services): add gotify push notification server

--- modules/gotify.nix
+++ modules/gotify.nix
@@ -6,11 +6,11 @@
   # Gotify - Self-hosted Push Notification Server
   services.gotify = {
     enable = true;
-    port = 8080;
 
     # We use a static environment file configuration here.
     # Gotify will create its SQLite database internally.
     environment = {
+      GOTIFY_SERVER_PORT = "8080";
       GOTIFY_DEFAULTUSER_PASS = "admin"; # CHANGE THIS AFTER FIRST LOGIN
       GOTIFY_SERVER_KEEPALIVEPERIODSECONDS = "0";
       GOTIFY_SERVER_SSL_ENABLED = "false"; # Nginx handles SSL
