546c64b83b69e6260fa1d78256231b270bd6a18f
Aloïs VINCENT <alois.vincent@imt-atlantique.net>
2026-05-03 17:44:41 +0200

    adjusted garbage collector settings to save space

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,6 @@
 # /Documents/nix-config/vps/configuration.nix
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   imports = [
     ./modules/adguard.nix
     ./modules/grafana.nix
@@ -23,20 +24,23 @@
   ];
 
   # Nix Configuration
-  nix.settings.trusted-users = [
-    "root"
-    "@wheel"
-  ];
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    auto-optimise-store = true; # Hardlinks duplicate files to save space
+  };
 
-  # Automatic Garbage Collection
+  # Aggressive Garbage Collection
   nix.gc = {
     automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
+    dates = "daily";
+    options = "--delete-older-than 3d";
   };
 
   # Utility to check if reboot is needed
@@ -64,7 +68,7 @@
     enable = true;
     efiSupport = true;
     efiInstallAsRemovable = true;
-    devices = ["/dev/sda"];
+    devices = [ "/dev/sda" ];
   };
 
   # Kernel modules for Hetzner cloud VMs (virtio drivers)
@@ -81,7 +85,7 @@
   time.timeZone = "Europe/Paris";
 
   # Secrets
-  age.identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+  age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   age.secrets.homepage-env.file = ./secrets/homepage-env.age;
   age.secrets.immich-env.file = ./secrets/immich-env.age;
   age.secrets.grafana-secret-key = {
@@ -101,7 +105,7 @@
         80
         443
       ];
-      trustedInterfaces = ["tailscale0"];
+      trustedInterfaces = [ "tailscale0" ];
     };
   };
 
@@ -148,7 +152,7 @@
 
   users.users.alois = {
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = [ "wheel" ];
     hashedPassword = "$y$j9T$njIq5wNQMvXeHHAgkkqRs.$BPmNgJG96aMqIU48AjXuElMy1xbU6tGmOCw4UTRhlt9";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINa1VOKGJI/j5mfvo5QsKk/tX+vNr3CdjdYYNfbPxdDK alois@fedora"
@@ -157,7 +161,7 @@
 
   users.users.deploy = {
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = [ "wheel" ];
     hashedPassword = "!"; # Keys only
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA/J/t0j3ylcxgXjMOfol8JL0RuuoKAjVvP3X+34o/DF github-actions-deploy"
@@ -169,11 +173,11 @@
   # Allow deploy user to run sudo without password (for CI/CD)
   security.sudo.extraRules = [
     {
-      users = ["deploy"];
+      users = [ "deploy" ];
       commands = [
         {
           command = "ALL";
-          options = ["NOPASSWD"];
+          options = [ "NOPASSWD" ];
         }
       ];
     }
