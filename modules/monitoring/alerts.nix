fb3dfea8cbd7fe493ee0addb023737d4ad4788f4
Aloïs VINCENT <alois.vincent@imt-atlantique.net>
2026-07-13 19:28:30 +0200

    chore: format.

--- modules/monitoring/alerts.nix
+++ modules/monitoring/alerts.nix
@@ -1,4 +1,8 @@
-{pkgs, config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   # Universal Systemd Failure Notification Service
   #
   # This service is triggered via `OnFailure=notify-failure@%n.service`
@@ -26,32 +30,32 @@
     # The %i gets replaced with the name of the service that failed.
     # We fetch the last 5 log lines of the crashed service to include in the Telegram message.
     script = ''
-      # Verify environment variables
-      if [ -z "$TELEGRAM_TOKEN" ] || [ -z "$TELEGRAM_CHAT_ID" ]; then
-        echo "Telegram credentials not found in agenix secrets. Skipping notification."
-        exit 0
-      fi
+            # Verify environment variables
+            if [ -z "$TELEGRAM_TOKEN" ] || [ -z "$TELEGRAM_CHAT_ID" ]; then
+              echo "Telegram credentials not found in agenix secrets. Skipping notification."
+              exit 0
+            fi
 
-      FAILED_SERVICE="%i"
+            FAILED_SERVICE="%i"
 
-      # Grab the last few log lines of the failed service
-      LOGS=$(journalctl -u "$FAILED_SERVICE" -n 5 --no-pager || true)
+            # Grab the last few log lines of the failed service
+            LOGS=$(journalctl -u "$FAILED_SERVICE" -n 5 --no-pager || true)
 
-      # Build HTML message
-      MESSAGE="🚨 <b>[VPS Notification Alert]</b> Service <b>$FAILED_SERVICE</b> has failed or crashed!
+            # Build HTML message
+            MESSAGE="🚨 <b>[VPS Notification Alert]</b> Service <b>$FAILED_SERVICE</b> has failed or crashed!
 
-<b>Host:</b> $(hostname)
-<b>Time:</b> $(date)
+      <b>Host:</b> $(hostname)
+      <b>Time:</b> $(date)
 
-<b>Last Logs:</b>
-<pre>$LOGS</pre>"
+      <b>Last Logs:</b>
+      <pre>$LOGS</pre>"
 
-      # Send HTML message via curl
-      curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
-        -d "chat_id=$TELEGRAM_CHAT_ID" \
-        --data-urlencode "text=$MESSAGE" \
-        -d "parse_mode=HTML" \
-        --fail-with-body
+            # Send HTML message via curl
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
+              -d "chat_id=$TELEGRAM_CHAT_ID" \
+              --data-urlencode "text=$MESSAGE" \
+              -d "parse_mode=HTML" \
+              --fail-with-body
     '';
   };
 }
